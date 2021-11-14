f2c83d51ed5b7ee6d7f59ab75cee0f5fa8878a16
Samuel Brook <samuelbhbrook@gmail.com>
2021-11-14 18:35:18 +0100

    Update main, index and styles files. Allows projects to be created in the sidebar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,44 @@
-import { makePage } from "./make-page";
+import { makePage } from "./dom-manipulation/render-page";
+import { addProjectButton, removeProjectButton } from "./dom-manipulation/toggle-add-project-button";
+import { addProject, inputProject } from "./dom-manipulation/render-project";
+import { Project } from "./logic/make-project";
+
+const clickAddProject = () => {
+    const addButton = document.querySelector("#add-projects");
+    addButton.addEventListener("click", () => {
+        removeProjectButton();
+        inputProject();
+    });
+    document.addEventListener("keydown", event => {
+        if (event.keyCode === 13) {
+            const text = document.querySelector("#project-input");
+            let projectName = text.value;
+            let newProject = new Project(projectName);
+            text.remove();
+            addProject(newProject.title);
+            addProjectButton();
+        }
+    })
+}
+
+const displayProject = () => {
+    // const project = document.querySelector(".project");
+    // project.addEventListener("click", () => {
+    //     let name = project.id;
+    //     console.log(name);
+    // })
+    const projects = document.querySelectorAll(".project");
+    projects.forEach(project => {
+        project.addEventListener("click", () => {
+            let name = project.id;
+            console.log(name);
+            renderProjectView(name);
+        });
+    })
+}
+
 
 
 makePage();
+clickAddProject();
+displayProject();
